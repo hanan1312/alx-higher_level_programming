04b7cd9895b54ff8c7499f766f2eaa6dedaaaded
hanan1312 <nana.elmogey@gmail.com>
2024-04-09 04:12:34 +0200

    js is beautiful

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,18 +1,5 @@
 #!/usr/bin/node
-// const myObject = {
-//   type: 'object',
-//   value: 12
-// };
-// console.log(myObject);
-// function incr(obj) {
-//   obj.value++;
-// }
-// myObject.incr();
-// console.log(myObject);
-// myObject.incr();
-// console.log(myObject);
-// myObject.incr();
-// console.log(myObject);
+
 const myObject = {
   type: 'object',
   value: 12,
